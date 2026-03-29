6fc72a2c8276d4d87e691e562ddacae1f159fb9a
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-03-30 00:17:06 +0530

    Rename Student class to StudentClass and update usage

--- src/StudentClass.java
+++ src/StudentClass.java
@@ -1,36 +1,40 @@
 import java.util.Scanner;
 
-class Student {
+class StudentClass {
     String name;
     int rollNo;
 
-    Student(String name, int rollNo) {
+    // Parameterized Constructor
+    StudentClass(String name, int rollNo) {
         this.name = name;
         this.rollNo = rollNo;
     }
 
+    // Method to display details
     void display() {
         System.out.println("Name: " + name + " Roll: " + rollNo);
     }
-}
 
-public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        String name1 = scanner.nextLine();
-        int roll1 = scanner.nextInt();
-        scanner.nextLine(); 
+        // Input for Student 1
+        String name1 = sc.nextLine();
+        int roll1 = sc.nextInt();
+        sc.nextLine(); // consume leftover newline
 
-        String name2 = scanner.nextLine();
-        int roll2 = scanner.nextInt();
+        // Input for Student 2
+        String name2 = sc.nextLine();
+        int roll2 = sc.nextInt();
 
-        Student student1 = new Student(name1, roll1);
-        Student student2 = new Student(name2, roll2);
+        // Creating objects using parameterized constructor
+        StudentClass s1 = new StudentClass(name1, roll1);
+        StudentClass s2 = new StudentClass(name2, roll2);
 
-        student1.display();
-        student2.display();
-        
-        scanner.close();
+        // Displaying details
+        s1.display();
+        s2.display();
+
+        sc.close();
     }
 }
